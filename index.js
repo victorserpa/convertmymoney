d6f94fac659edabe45855b5816e0bc2d75671b43
Victor Schmucker Serpa <71465529+schmvitor@users.noreply.github.com>
2021-05-05 19:28:35 -0300

    initial version.

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const convert = require('./lib/convet')
+const apiBCB = require('./lib/api-bcb')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', async(req, res) => {
+  const cotacao = await apiBCB.getCotacao()
+  console.log(cotacao)
+  res.render('home', {
+    cotacao
+  })
+})
+
+app.get('/cotacao', (req, res) => {
+    const { cotacao, quantidade } = req.query
+    if(cotacao && quantidade){
+    const conversao = convert.convert(cotacao, quantidade)
+      res.render('cotacao', {
+        error: false,
+        cotacao: convert.toMoney(cotacao),
+        quantidade: convert.toMoney(quantidade),
+        conversao: convert.toMoney(conversao)
+      })
+    }else{
+      res.render('cotacao', {
+        error:'Valores inválidos'
+      })
+    }
+})
+
+
+app.listen(3000, err => {
+  if(err){
+    console.log('Não foi possivel iniciar.')
+  }else{
+    console.log('ConvertMyMoney esta online.')
+  }
+})
